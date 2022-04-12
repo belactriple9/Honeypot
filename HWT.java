cdf4b164a6ef737757b636c668e00d1ee2a6ba27
belactriple9 <carippey1@gmail.com>
2022-04-12 16:29:54 -0400

    added SMTP messages

--- HWT.java
+++ HWT.java
@@ -8,11 +8,13 @@ class HWT extends Thread {
 	String pwd=null, hattempt=null;
 	String so0=null, so1=null, so2=null;
 
+	String smtpGreeting="220 ubuntu-s-1vcpu-2gb-amd-nyc1-01 ESMTP OpenSMTPD";
+	String smtpError="500 5.5.1 Invalid command: Command unrecognized";
+
 	HWT (Socket sck) {
 		s=sck;
-		so0 = new String("root@68.183.19.99's password: ");
-		so1 = new String("Permission denied, please try again.");
-		so2 = new String("Welcome to Ubuntu 18.04.3 LTS (GNU/Linux 4.15.0-166-generic x86_64)\n * Documentation:  https://help.ubuntu.com\n * Management:     https://landscape.canonical.com\n * Support:        https://ubuntu.com/advantage\n\n * Canonical Livepatch is available for installation.\n - Reduce system reboots and improve kernel security. Activate at:\n https://ubuntu.com/livepatch\n\n 576 packages can be updated.\n 0 updates are security updates.\n\n\n\n\n\n Last login: Wed Apr  6 15:34:44 2022 from 137.45.192.100\n root@itecE:~# ");
+	
+
 
 		System.out.printf("honey thread created");
 	}
@@ -30,11 +32,12 @@ class HWT extends Thread {
 			BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
 
-			bw.write(so0, 0, so0.length()); bw.newLine(); bw.flush();
+			bw.write(smtpGreeting, 0, smtpGreeting.length()); bw.newLine(); bw.flush();
+			// read bytes into br
 			pwd=br.readLine();
 			System.out.println("pwd: " + pwd);
 
-			bw.write(so2, 0, so2.length()); bw.newLine(); bw.flush();
+			bw.write(smtpError, 0, smtpError.length()); bw.newLine(); bw.flush();
 
 			//while (true) {
 			for (int i=0; i<1000; i++) {
