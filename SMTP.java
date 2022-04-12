20a81ffd62ccb4a4e64ed2dd5f29f4bda03990f9
ngmi <ngmi@protonmail.com>
2022-04-12 22:53:42 +0000

    moved SMTP to a new class/driver

---
+++ SMTP.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.Date;
+import java.text.*;
+import java.net.*;
+
+class SMTP extends Thread {
+	Socket s;
+	String pwd=null, hattempt=null;
+	String so0=null, so1=null, so2=null;
+
+	String smtpGreeting="220 ubuntu-s-1vcpu-2gb-amd-nyc1-01 ESMTP OpenSMTPD";
+	String smtpError="500 5.5.1 Invalid command: Command unrecognized";
+	String smtpOK="250 2.0.0 OK";
+
+	SMTP (Socket sck) {
+		s=sck;
+	
+
+
+		System.out.printf("honey thread created");
+	}
+
+
+
+	public void run() {
+		try {
+			Date d = new Date();
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+			System.out.println("honey thread starts at: " + formatter.format(d));
+
+
+			//java stuff, take the tin cans out of the box
+			BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+
+			bw.write(smtpGreeting, 0, smtpGreeting.length()); bw.newLine(); bw.flush();
+			// read bytes into br
+			pwd=br.readLine();
+			System.out.println("pwd: " + pwd);
+
+			bw.write(smtpOK, 0, smtpOK.length()); bw.newLine(); bw.flush();
+
+			// wait until the client closes the connection
+			while(true) {
+				hattempt=br.readLine();
+				System.out.println("hattempt: " + hattempt);
+				if (hattempt.equals("QUIT")) {
+					bw.write(smtpGreeting, 0, smtpGreeting.length()); bw.newLine(); bw.flush();
+					break;
+				}
+				else {
+					bw.write(smtpError, 0, smtpError.length()); bw.newLine(); bw.flush();
+				}
+			}
+
+			s.close();
+		} catch (Exception e) {
+			//e.printStackTrace();
+		}
+	}
+}
---
+++ SMTPDriver.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+
+
+class SMTPDriver {
+	 public static void main(String[] args) {
+			 ServerSocket ss=null;
+			 Socket s=null;
+
+			 if (args.length != 1) {
+				 System.out.printf("usage: java SMTPDriver portNumber\n");
+				 System.exit(1);
+			 }
+
+			 try {
+			 	ss = new ServerSocket(Integer.parseInt(args[0]));
+		 	} catch (Exception e) {
+			 	e.printStackTrace();
+		 	}
+
+			 while (true) {
+				 try {
+					System.out.printf("wait for an incoming connection\n");
+			 		s = ss.accept();
+			 		System.out.printf("SMTPDriver: received an incoming connection\n");
+			 		SMTP worker = new SMTP(s);
+					worker.start();
+				 } catch (Exception e2) {
+					 e2.printStackTrace();
+					 try {Thread.sleep(5000);} catch (Exception en){}
+				 }
+			 }
+	 }
+}
